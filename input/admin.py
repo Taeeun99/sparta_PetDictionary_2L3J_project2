cf132c8f49c655d9fc626b4a442e2a43ce85a05d
Taeeun99 <gfhjkuji@naver.com>
2022-10-18 03:49:25 +0900

    reserch img 파일 데이터 베이스에 올리기 및 ERD 작업 기초

--- input/admin.py
+++ input/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import InputModel
+from .models import ResearchModel
 # Register your models here.
+
+# 나의 InputModel을 Admin에 추가 해 줍니다
+admin.site.register(InputModel)
--- input/views.py
+++ input/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import InputModel
+import os
 # Create your views here.
 
 def main(request):
@@ -8,10 +9,18 @@ def main(request):
     
 
 def input(request):
+    
     return render(request, 'input.html')
 
+
 def output(request):
+    text = request.POST.get("test")
+    img = request.FILES.get("imgfile")
+    print(img, text)
+    my_search = InputModel.objects.create(imgfile=img)  # 이미지 저장
+    my_search.save()
     return render(request, 'output.html')
+
 
 def if_worng(request):
     return render(request, 'if_worng.html')
