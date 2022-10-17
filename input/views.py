972e5dac833c5bf2814d8ba05b5e64c1c20cf0ed
JoonPyoJang <jjpyo1009@gmail.com>
2022-10-17 23:14:41 +0900

    templates 수정

--- input/views.py
+++ input/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def main(request):
+    return render(request, 'main.html')
+    
+
+def input(request):
+    return render(request, 'input.html')
+
+def output(request):
+    return render(request, 'output.html')
+
+def if_worng(request):
+    return render(request, 'if_worng.html')
+
+def graph(request):
+    return render(request, 'accuracy_graph.html')
