9810fcea41b1f00b48298fd5f2e567090a6fa65d
jihyun-cho-0 <roseview808@gmail.com>
2022-10-18 14:26:55 +0900

    update - search

--- input/views.py
+++ input/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import InputModel, ResearchModel
 
 # Create your views here.
 
@@ -10,29 +11,47 @@ def main(request):
 def input(request):
     return render(request, 'input.html')
 
+
 def output(request):
-    if request.method = 'GET':
-        keyword = Input.objects.get('pet') # DB에서 이미지 분석 라벨값 호출
+    img = request.FILES.get("imgfile")
+    InputModel.objects.create(imgfile=img)  # 이미지 수신 후 저장
+    # 머신러닝 실행 
+    # 결과값(정답라벨) DB에 저장 
+    
+    if request.method == 'POST': # 1번째 POST
+        # keyword = InputModel.objects.get('pet') # DB에서 이미지 분석 라벨값 호출
+        keyword = 'cat-ddd' # DB에서 이미지 분석 라벨값 호출
         search_link = "https://www.google.com/search?q="+keyword # 구글 검색 url
-        return render(request, 'search.html', {'search_link':search_link})
+        
+        top_category = str(keyword.split('-')[:1])
+        low_category = str(keyword.split('-')[1:])
 
-    elif request.method = 'POST': # 설문 결과에 따라 redirect (Y 설문결과 페이지, N 재검색 페이지)
-        text = request.POST.get("test")
-        img = request.FILES.get("imgfile")
-        print(img, text)
-        my_search = InputModel.objects.create(imgfile=img)  # 이미지 저장
-        # my_search.save()
+        context = {
+            'search_link':search_link,
+            'top_category':top_category.strip("'" "[" "]"),
+            'low_category':low_category.strip("'" "[" "]"),
+        }
+        return render(request, 'output.html', context)
 
-        if request.POST.get('correct') == 'yes': # 정확도 설문 답변이 yes라면 -> html에서 value="yes" 지정 필요
-            Research.create(correct = True)
-            return redirect('/') # 설문 결과 페이지로 연결
+    elif request.method == 'POST': # 2번째 POST
 
-        elif request.POST.get('correct') == 'no': # 정확도 설문 답변이 no라면
-            Research.objects.create(correct = False) # 
-            return redirect('/') # 재검색 페이지로 연결
+        if request.POST.get('correct') == 'yes': # 정확도 설문 답변이 value="yes"라면 
+            my_correct = ResearchModel.objects.create(correct=True)
+            print(my_correct.get('correct'))     #필드값 생성 추가
+            return redirect('/graph')
+
+        elif request.POST.get('correct') == 'no':
+            my_correct =ResearchModel.objects.create(correct=False)
+            print(my_correct.get('correct'))
+            return redirect('/worng')
+        
+        else:
+            return redirect('/graph')
+
 
 def if_worng(request):
     return render(request, 'if_worng.html')
+
 
 def graph(request):
     return render(request, 'accuracy_graph.html')
--- petdictionary/settings.py
+++ petdictionary/settings.py
@@ -32,14 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'input',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
     'input',
 ]
 
